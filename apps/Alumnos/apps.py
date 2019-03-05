edd8e8836d825055f1cbdfcab2613273706e92d1
anibal sanchez <anibalsanches733@gmail.com>
2019-03-05 17:20:23 -0600

    se agrego el primer nombre de alumno a la base de datos

--- apps/Alumnos/apps.py
+++ apps/Alumnos/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class AlumnosConfig(AppConfig):
-    name = 'Alumnos'
+# class AlumnosConfig(AppConfig):
+#     name = 'Alumnos'
--- apps/Alumnos/models.py
+++ apps/Alumnos/models.py
@@ -1,7 +1,3 @@
-# from xmlrpclib import DateTime
-
-# from pip import models
-
 from django.db import models
 
 
@@ -9,7 +5,7 @@ from django.db import models
 class Alumnos(models.Model):
     nombre = models.CharField(max_length=200, help_text = 'Nombre(s)')
     a_paterno = models.CharField(max_length=200, help_text = 'Apellido Paterno')
-    a_materno =  models.CharField(max_lenght=200, help_text = 'Apellido Materno')
+    a_materno =  models.CharField(max_length=200, help_text = 'Apellido Materno')
     f_nacimiento = models.DateTimeField(blank=True, null=True, help_text = 'Fecha de nacimiento')
 
     def __srt__(self):
